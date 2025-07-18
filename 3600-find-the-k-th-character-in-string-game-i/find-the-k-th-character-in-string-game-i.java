ccb5fa0f46def10e73e159a3bc2edef088890235
Tan Nguyen <109587630+TanNguyen17112003@users.noreply.github.com>
2025-07-19 02:43:58 +0700

    Time: 0 ms (100.00%) | Memory: 41.3 MB (92.95%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,28 +1,27 @@
 class Solution {
     public char kthCharacter(int k) {
-        String result = "a";
-        int numberOfAddition = 0;
-        while (Math.pow(2, numberOfAddition) < k) {
-            numberOfAddition++;
+        // Tìm độ sâu nhỏ nhất n sao cho 2^n >= k
+        int depth = 0;
+        while ((1 << depth) < k) {
+            depth++;
         }
-        for (int i = 0; i < numberOfAddition; i++) {
-            StringBuilder newString = new StringBuilder();
-            for (int j = 0; j < result.length(); j++) {
-                char nc = nextChar(result.charAt(j));
-                newString.append(nc);
-            }
-            result += newString.toString();
+        return findChar(depth, k);
+    }
+
+    private char findChar(int level, int k) {
+        if (level == 0) return 'a';  // S_0 = "a"
+        
+        int half = 1 << (level - 1); // 2^(level - 1)
+
+        if (k <= half) {
+            return findChar(level - 1, k);
+        } else {
+            char prev = findChar(level - 1, k - half);
+            return nextChar(prev);
         }
-        return result.charAt(k-1);
     }
 
     private char nextChar(char c) {
-    if (c >= 'a' && c <= 'z') {
         return (c == 'z') ? 'a' : (char)(c + 1);
     }
-    if (c >= 'A' && c <= 'Z') {
-        return (c == 'Z') ? 'A' : (char)(c + 1);
-    }
-    throw new IllegalArgumentException("Input must be a letter (a-z or A-Z)");
-}
 }
