745155485e69d7616856e1dcb10ea557769ad767
Tan Nguyen <109587630+TanNguyen17112003@users.noreply.github.com>
2025-07-19 17:58:42 +0700

    Time: 2 ms (65.03%) | Memory: 43.2 MB (11.33%) - LeetSync

--- 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
+++ 241-different-ways-to-add-parentheses/different-ways-to-add-parentheses.java
@@ -1,32 +1,27 @@
-public class Solution {
-    Map<String, List<Integer>> memo = new HashMap<>();
-
-    public List<Integer> diffWaysToCompute(String input) {
-        if (memo.containsKey(input)) return memo.get(input);
-
+class Solution {
+    public List<Integer> diffWaysToCompute(String expression) {
         List<Integer> result = new ArrayList<>();
+        
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == '+' || c == '-' || c == '*' || c == '/') {
+                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right = diffWaysToCompute(expression.substring(i+1));
 
-        for (int i = 0; i < input.length(); i++) {
-            char c = input.charAt(i);
-            if (c == '+' || c == '-' || c == '*') {
-                List<Integer> left = diffWaysToCompute(input.substring(0, i));
-                List<Integer> right = diffWaysToCompute(input.substring(i + 1));
-
-                for (int l : left) {
-                    for (int r : right) {
-                        if (c == '+') result.add(l + r);
-                        else if (c == '-') result.add(l - r);
-                        else if (c == '*') result.add(l * r);
+                for (int l: left) {
+                    for (int r: right) {
+                        if (c == '+') result.add(l+r);
+                        else if (c == '-') result.add(l-r);
+                        else if (c == '*') result.add(l*r);
+                        else result.add(l/r);
                     }
                 }
             }
         }
 
         if (result.isEmpty()) {
-            result.add(Integer.parseInt(input));
+            result.add(Integer.parseInt(expression));
         }
-
-        memo.put(input, result);
         return result;
     }
 }
